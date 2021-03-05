99fc1f6910409ce83eff0740139d11ed284eb172
Dinesh Tyagi <dinesh777tyagi@gmail.com>
2021-03-05 23:30:28 +0530

    First commit

---
+++ StudentManagementApp/src/com/student/manage/Start.java
@@ -0,0 +1,82 @@
+package com.student.manage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Start {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Welcome to Student Management App");
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		while(true)
+			{
+			System.out.println("Press 1 to Add Student");
+			System.out.println("Press 2 to Delete Student");
+			System.out.println("Press 3 to Update Student");
+			System.out.println("Press 4 to Display Student");
+			System.out.println("Press 5 to exit app");
+			
+			int c = Integer.parseInt(br.readLine());
+			
+			if(c==1)
+			{
+				//add student
+				System.out.println("Enter user name");
+				String name=br.readLine();
+				
+				System.out.println("Enter user phone");
+				String phone=br.readLine();
+				
+				System.out.println("Enter user City");
+				String city=br.readLine();
+				
+				//create student object to store student
+				Student st= new Student(name, phone, city);
+				boolean answer= StudentDao.insertStudentToDB(st);
+				if(answer)
+				{
+					System.out.println("Student is added successfully...");
+				}
+				else{
+					System.out.println("Something went wrong try again...");
+				}
+				System.out.println(st);
+			}else if(c==2)
+			{
+				//delete student
+				
+				System.out.println("Enter student id to delete:");
+				int userId = Integer.parseInt(br.readLine());
+				boolean f =StudentDao.deleteStudent(userId);
+				if(f)
+				{
+					System.out.println("Student is deleted..");
+				}
+				else{
+					System.out.println("Something went wrong ...");
+				}
+			} else if(c==3)
+			{
+				//update student
+			}else if(c==4)
+			{
+				//display student
+				StudentDao.showAllStudent();
+			}else if(c==5)
+			{
+				//exit
+				break;
+			} 
+			else{
+				
+			}
+			
+			}
+		System.out.println("Thank you for using my application...");
+		System.out.println("See you soon...");
+
+	}
+
+}
